f38ecbc329c1fec7e58ce05c78fc85cf3669c2bf
JeyalakshmiSelvam <jeyalakshmi.selvam@ideas2it.com>
2022-12-06 20:30:30 +0530

    Middleware changes

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,30 @@
 // eslint-disable-next-line no-unused-vars
+const {unless} = require('express-unless');
+const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
+
+  let validateToken= async function   (req, res, next) {
+    if(req.headers.authorization){
+      try{
+        let data = await app.service('authentication').verifyAccessToken(req.headers.authorization,app.get["authentication.secret"])
+        // let payload = await JWTStrategy.authenticate()s
+        console.log("data",data)
+        next ()
+      }catch(err){
+        return err
+      }
+    }else{
+      return;
+    }
+  }
+
+  validateToken['unless'] = unless;
+  app.use(validateToken['unless']({
+    path: [
+      '/authentication',
+      { url: '/users', methods: ['POST'] }
+    ]
+  }));
 };
